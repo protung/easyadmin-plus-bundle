f78e11e97f1bd380f101cfcaa4e879e34f2c6738
Dragos Protung <dragos@protung.de>
2023-02-09 12:44:56 +0100

    Disallow EasyAdminBundle 4.5.2 because of Enum support

--- src/ProtungEasyAdminPlusBundle.php
+++ src/ProtungEasyAdminPlusBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Protung\EasyAdminPlusBundle;
 
+use Protung\EasyAdminPlusBundle\DependencyInjection\CompilerPass\EasyAdminMenuItemMatcher;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class ProtungEasyAdminPlusBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new EasyAdminMenuItemMatcher());
+    }
 }
--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -6,13 +6,16 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldConfiguratorInterface;
 use EasyCorp\Bundle\EasyAdminBundle\DependencyInjection\EasyAdminExtension;
+use EasyCorp\Bundle\EasyAdminBundle\Menu\MenuItemMatcher;
+use EasyCorp\Bundle\EasyAdminBundle\Menu\MenuItemMatcherInterface;
 use Symfony\Component\Form\FormTypeInterface;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services()
         ->defaults()->private()->autowire()
         ->instanceof(FieldConfiguratorInterface::class)->tag(EasyAdminExtension::TAG_FIELD_CONFIGURATOR)
-        ->instanceof(FormTypeInterface::class)->tag('form.type');
+        ->instanceof(FormTypeInterface::class)->tag('form.type')
+        ->alias(MenuItemMatcherInterface::class, MenuItemMatcher::class);
 
     $services
         ->load('Protung\\EasyAdminPlusBundle\\', '../../../src/*')
