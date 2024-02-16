4924488cd1762a0e81eb86463aa570c38a5e48a8
Dragos Protung <dragos@protung.de>
2024-02-16 13:21:49 +0100

    Improved URL generation for dashboard controller tests

--- src/Test/Controller/DashboardControllerTestCase.php
+++ src/Test/Controller/DashboardControllerTestCase.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Protung\EasyAdminPlusBundle\Test\Controller;
 
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Psl\Dict;
 use Psl\Iter;
-use Psl\Str;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 use function count;
 use function is_countable;
@@ -21,24 +21,14 @@ abstract class DashboardControllerTestCase extends AdminWebTestCase
 {
     /**
      * @param iterable<string,string|null> $expectedMenuItems
+     * @param array<mixed>                 $routeParameters
      */
-    protected function assertMenu(iterable $expectedMenuItems): void
+    protected function assertMenu(iterable $expectedMenuItems, array $routeParameters = []): void
     {
-        $router = $this->getContainerService(Router::class, 'router');
-
-        $dashboardRoutes = Dict\filter(
-            $router->getRouteCollection()->all(),
-            fn (Route $route): bool => Str\starts_with((string) $route->getDefault('_controller'), $this->getDashboardControllerFqcn())
-        );
-
-        $dashboardRouteName = Iter\first_key($dashboardRoutes);
-
-        self::assertIsString($dashboardRouteName, 'Could not find route for the dashboard controller.');
-
         $client                  = $this->getClient();
         $originalFollowRedirects = $client->isFollowingRedirects();
         $client->followRedirects();
-        $crawler = $client->request(Request::METHOD_GET, $router->generate($dashboardRouteName));
+        $crawler = $client->request(Request::METHOD_GET, $this->prepareDashboardUrl($routeParameters));
         $client->followRedirects($originalFollowRedirects);
 
         self::assertGreaterThan(0, $crawler->filter('#main-menu ul.menu')->count(), 'Menu DOM element does not exist.');
@@ -66,6 +56,31 @@ abstract class DashboardControllerTestCase extends AdminWebTestCase
             self::assertSame($label, $menuElement->text(normalizeWhitespace: true));
             self::assertSame($value, $url);
         }
+    }
+
+    /**
+     * @param array<mixed> $routeParameters
+     */
+    protected function assertRequestGet(
+        array $routeParameters = [],
+        int $expectedResponseStatusCode = Response::HTTP_OK,
+    ): Crawler {
+        $crawler = $this->getClient()->request(Request::METHOD_GET, $this->prepareDashboardUrl($routeParameters));
+
+        self::assertResponseStatusCode($this->getClient()->getResponse(), $expectedResponseStatusCode);
+
+        return $crawler;
+    }
+
+    /**
+     * @param array<mixed> $routeParameters
+     */
+    protected function prepareDashboardUrl(array $routeParameters): string
+    {
+        return $this->getContainerService(AdminUrlGenerator::class)
+            ->setAll(Dict\sort_by_key($routeParameters))
+            ->setDashboard($this->getDashboardControllerFqcn())
+            ->generateUrl();
     }
 
     /**
