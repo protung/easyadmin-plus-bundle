df2abdb1e323ae8bd00e9a6804c2e15ee3542980
Dragos Protung <dragos@protung.de>
2024-06-03 10:37:34 +0200

    Fixed deprecation

--- src/DependencyInjection/ProtungEasyAdminPlusExtension.php
+++ src/DependencyInjection/ProtungEasyAdminPlusExtension.php
@@ -6,8 +6,8 @@ namespace Protung\EasyAdminPlusBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class ProtungEasyAdminPlusExtension extends Extension
 {
